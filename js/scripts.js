135975dd548d418a37fea6fa35740cda5db2c19f
Iwona <iw.stasik@gmail.com>
2016-12-10 19:13:13 +0100

    zadanie 8.4

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt('Wpisz dowolną cyfrę'),
+	h = prompt('Wpisz kolejną dowolną cyfrę'),
+	triangleArea = a*h/2;
+alert('Pole trójkąta o podstawie ' + a + ' i wysokości ' + h + ' wynosi: ' + triangleArea)
+console.log(triangleArea);
